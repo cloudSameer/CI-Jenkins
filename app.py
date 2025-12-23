127393792ce03567dafbe0d9b730f1844f40a5f7
sameer <akkalkotsameer0@gmail.com>
2025-12-23 06:28:44 +0000

    Initial commit: app code and tests

---
+++ app.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    result = a + b
+    return result
+
+
+def subtract(a, b):
+    result = a - b
+    return result
+
+
+def show_message():
+    message = "CI pipeline is working"
+    return message
